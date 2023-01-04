36296e6f78e66ed582bdf32de5f421b7aa4499ad
Marcos-Antunes-Junior <mantunes140397@gmail.com>
2023-01-03 21:14:40 -0300

    Completed prep 1 and prep 2.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -3,7 +3,45 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
+    {   
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+        string letter = "";
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if(number < 90 && number >= 80)
+        {
+            letter = "B";
+        }
+        else if(number < 80 && number >= 70)
+        {
+            letter = "C";
+        }
+        else if(number < 70 && number >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (number < 70)
+        {
+            
+            Console.WriteLine("");
+            Console.WriteLine("You were reproved!");
+        }
+        else
+        {
+            Console.WriteLine("You were approved!");
+        }
+
+        
     }
 }
