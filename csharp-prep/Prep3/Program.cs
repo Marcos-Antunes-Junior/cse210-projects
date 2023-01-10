a5e04ffbf277ad15d8b103e2438c41cdde3a8a86
Marcos-Antunes-Junior <mantunes140397@gmail.com>
2023-01-10 14:06:26 -0300

    Completed Prep3 and Prep4.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,57 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   
+        string answer;
+        do{
+
+         int input;
+         int number;
+         Random randomGenerator = new Random();
+         number = randomGenerator.Next(1, 11);
+        do
+        {
+            
+            
+            Console.Write("What is your guess? ");
+            input = int.Parse(Console.ReadLine());
+
+            if (input < number)
+            {
+               Console.WriteLine("Higher.");
+            }else if (input > number)
+            {
+                Console.WriteLine("Lower.");
+            }else 
+            {
+                Console.WriteLine("You guessed it!");
+
+            }
+
+
+        
+        
+        
+        
+        
+        
+        
+        }while (input < number || input > number);
+        
+        Console.WriteLine("Do you want to continue? ");
+        answer = Console.ReadLine();
+        }while (answer == "yes"); 
+
     }
+
 }
+
+
+
+        
+         
+        
+        
+        
+        
+        
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+    {   int number;
+        List<int> numberList = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        do
+        {   
+            
+            
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            
+            if (number != 0)
+            {
+               numberList.Add(number);
+            }
+            
+
+            
+            
+        }while (number != 0);
+            
+            
+            int sum = numberList.AsQueryable().Sum();
+            float average = ((float)sum) / numberList.Count;
+            int max = numberList.Max();
+
+            Console.WriteLine("The sum is: " + sum);
+            Console.WriteLine("The average is: " + average);
+            Console.WriteLine("The largest number is: " + max);
+            
+            
+
     }
 }
